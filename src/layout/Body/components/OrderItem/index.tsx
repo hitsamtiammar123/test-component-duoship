80a97d11171a0b5b65d524c28120344cf7e78d02
Hitsam Tiammar <hitsamtiammmar@gmail.com>
2023-01-26 20:46:32 +0700

    implement props data

--- src/layout/Body/components/OrderItem/index.tsx
+++ src/layout/Body/components/OrderItem/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
+import moment from 'moment';
+import numbro from 'numbro';
 import { Grid, Accordion as BaseAccordion, AccordionSummary as BaseAccordionSummary, AccordionDetails as BaseAccordionDetails, Button } from '@mui/material';
 import './styles.scss';
 
@@ -53,9 +55,26 @@ const RightButton = styled(Button)({
   marginBottom: '23px'
 })
 
-export default function OrderItem(){
+export type ItemBoughtProps = {
+  itemId: number,
+  name: string,
+  purchasedDistributor: string,
+  eligibleThrough: Date | string,
+  quantity: number
+}
+
+export type OrderItemProps = {
+    orderPlaced: string | Date,
+    totalPrice: number,
+    shippedTo: string,
+    orderId: number,
+    delivered: Date | null,
+    items: Array<ItemBoughtProps>
+}
+
+export default function OrderItem(props: OrderItemProps){
+  const { orderId, orderPlaced, totalPrice, shippedTo, delivered, items } = props;
   function onLinkClicked(e: React.MouseEvent<HTMLAnchorElement>){
-    console.log({ e });
     e.stopPropagation();
   }
 
@@ -66,20 +85,20 @@ export default function OrderItem(){
         <Grid item spacing={2} container direction="row" xs>
           <Grid item container xs={3} direction="column">
             <span className="text">ORDER PLACED</span>
-            <span className="text-sm mt-7px">April 25, 2022</span>
+            <span className="text-sm mt-7px">{moment(orderPlaced).format('MMMM DD, YYYY')}</span>
           </Grid>
           <Grid item container xs={3} direction="column">
             <span className="text">TOTAL PRICE</span>
-            <span className="text-sm mt-7px">$3,555.35</span>
+            <span className="text-sm mt-7px">${numbro(totalPrice).format({ thousandSeparated:true })}</span>
           </Grid>
           <Grid item container xs={3} direction="column">
             <span className="text">SHIPPED TO</span>
-            <span className="text-sm text-purple mt-7px">Rebecca Williams</span>
+            <span className="text-sm text-purple mt-7px">{shippedTo}</span>
           </Grid>
         </Grid>
         <Grid item xs={3}>
           <Grid item container direction="row">
-            <span className="text">ORDER # 205.1</span>
+            <span className="text">ORDER # {orderId}</span>
           </Grid>
           <Grid item container justifyContent="space-between" direction="row">
             <a onClick={onLinkClicked} href="/#order-detail" className="text-sm text-purple mt-7px">Order Detail</a>
@@ -92,45 +111,28 @@ export default function OrderItem(){
     <AccordionDetails>
       <Grid direction="row" className="accordion-detail" container>
           <Grid item container direction="column" xs>
-            <span className="text extrabold main-title">Delivered October 25</span>
-            <Grid className="accordion-item-detail" container direction="row">
-              <Grid item container direction="column" xs>
-                <span className="text text-black bold">Meta Quest 2 — Advanced All-In-One Virtual Reality Headset — 128 GB</span>
-                <span className="text-black text-sm mt-7px">
-                  Purchased Distributor: <span className="text-purple light">Eliquid.com</span>
-                </span>
-                <span className="text-black text-sm">
-                  Return or replace items: Eligible through January 31, 2023
-                </span>
-                <span className="text-black text-sm light">
-                  Quantity : x2
-                </span>
-              </Grid>
-              <Grid item container xs={2}>
-                <ViewItemButton  color="secondary" variant="contained">
-                  <span className="text-sm">View Item</span>
-                </ViewItemButton>
-              </Grid>
-            </Grid>
-            <Grid className="accordion-item-detail" container direction="row">
-              <Grid item container direction="column" xs>
-                <span className="text text-black bold">PS5 Console- Horizon Forbidden West Bundle</span>
-                <span className="text-black text-sm mt-7px">
-                  Purchased Distributor: <span className="text-purple light">Eliquid.com</span>
-                </span>
-                <span className="text-black text-sm">
-                  Return or replace items: Eligible through January 31, 2023
-                </span>
-                <span className="text-black text-sm light">
-                  Quantity : x1
-                </span>
-              </Grid>
-              <Grid item container xs={2}>
-                <ViewItemButton  color="secondary" variant="contained">
-                  <span className="text-sm">View Item</span>
-                </ViewItemButton>
-              </Grid>
-            </Grid>
+            <span className="text extrabold main-title">Delivered {moment(delivered).format('MMMM DD')}</span>
+            {items.map(item => (
+                <Grid key={item.itemId} className="accordion-item-detail" container direction="row">
+                  <Grid item container direction="column" xs>
+                    <span className="text text-black bold">{item.name}</span>
+                    <span className="text-black text-sm mt-7px">
+                      Purchased Distributor: <span className="text-purple light">{item.purchasedDistributor}</span>
+                    </span>
+                    <span className="text-black text-sm">
+                      Return or replace items: Eligible through {moment(item.eligibleThrough).format('MMMM DD, YYYY')}
+                    </span>
+                    <span className="text-black text-sm light">
+                      Quantity : x{item.quantity}
+                    </span>
+                  </Grid>
+                  <Grid item container xs={2}>
+                    <ViewItemButton  color="secondary" variant="contained">
+                      <span className="text-sm">View Item</span>
+                    </ViewItemButton>
+                  </Grid>
+                </Grid>
+            ))}
           </Grid>
           <Grid item container className="right-bar" direction="column" xs={3}>
             <RightButton color="secondary" variant="contained">
--- src/layout/Body/index.tsx
+++ src/layout/Body/index.tsx
@@ -1,14 +1,78 @@
 import React from 'react';
 import { Grid } from '@mui/material';
 import { OrderItem } from './components';
+import moment from 'moment';
+import { OrderItemProps } from './components/OrderItem'
 import './styles.scss';
+
+const DUMMY_DATA: Array<OrderItemProps> = [
+  {
+    orderId: 205,
+    delivered: moment('2021-04-05', 'YYYY-MM-DD').toDate(),
+    items: [
+      {
+        itemId: 1,
+        name: 'Meta Quest 2 — Advanced All-In-One Virtual Reality Headset — 128 GB',
+        eligibleThrough: moment('2023-12','YYYY-MM').toDate(),
+        purchasedDistributor: 'eliquid.com',
+        quantity: 1
+      },
+      {
+        itemId: 2,
+        name: 'PS5 Console- Horizon Forbidden West Bundle',
+        eligibleThrough: moment('2024-01','YYYY-MM').toDate(),
+        purchasedDistributor: 'eliquid.com',
+        quantity: 2
+      },
+      {
+        itemId: 3,
+        name: 'Barang Jadi-jadian hehe',
+        eligibleThrough: moment('2024-01','YYYY-MM').toDate(),
+        purchasedDistributor: 'eliquid.com',
+        quantity: 2
+      },
+    ],
+    orderPlaced: moment('2022-04-05', 'YYYY-MM-DD').toDate(),
+    totalPrice: 3555,
+    shippedTo: 'Rebecca Williams'
+  },
+  {
+    orderId: 206,
+    delivered: moment('2021-05-12', 'YYYY-MM-DD').toDate(),
+    items: [
+      {
+        itemId: 1,
+        name: 'Meta Quest 2 — Advanced All-In-One Virtual Reality Headset — 128 GB',
+        eligibleThrough: moment('2023-12','YYYY-MM').toDate(),
+        purchasedDistributor: 'eliquid.com',
+        quantity: 1
+      },
+      {
+        itemId: 2,
+        name: 'PS5 Console- Horizon Forbidden West Bundle',
+        eligibleThrough: moment('2024-01','YYYY-MM').toDate(),
+        purchasedDistributor: 'eliquid.com',
+        quantity: 2
+      },
+      {
+        itemId: 3,
+        name: 'Barang Jadi-jadian hehe',
+        eligibleThrough: moment('2024-01','YYYY-MM').toDate(),
+        purchasedDistributor: 'eliquid.com',
+        quantity: 2
+      },
+    ],
+    orderPlaced: moment('2022-12-05', 'YYYY-MM-DD').toDate(),
+    totalPrice: 5552,
+    shippedTo: 'Rebecca Williams'
+  },
+]
 
 
 export default function Body(){
   return (
     <Grid container>
-        <OrderItem />
-        <OrderItem />
+        {DUMMY_DATA.map(item => <OrderItem key={item.orderId} {...item}/>)}
     </Grid>
   )
 }
